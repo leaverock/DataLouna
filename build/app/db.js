d579086eb939a91d6c1f9fb01298292fe0590679
Yaroschuk Denis <minor3994@rambler.ru>
2025-03-12 19:48:56 +0300

    Fix .gitignore

--- build/app/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const postgres_1 = __importDefault(require("postgres"));
-const local_json_1 = __importDefault(require("../../config/local.json"));
-const sql = (0, postgres_1.default)(Object.assign({}, local_json_1.default.pg));
-exports.default = sql;
